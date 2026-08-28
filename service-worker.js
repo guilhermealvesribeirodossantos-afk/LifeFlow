cbe659c6ab1fc6135b14c111ac79348e16463d9d
guilhermealvesribeirodossantos-afk <guilhermealvesribeirodossantos@gmail.com>
2026-08-28 19:01:07 -0300

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,37 @@
+const CACHE = "lifeflow-v6-7-202608281815";
+const ASSETS = [
+  "./",
+  "./index.html",
+  "./style.css?v=202608281815",
+  "./app.js?v=202608281815",
+  "./manifest.webmanifest?v=202608281815"
+];
+
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE)
+      .then(cache => cache.addAll(ASSETS))
+      .then(() => self.skipWaiting())
+  );
+});
+
+self.addEventListener("activate", event => {
+  event.waitUntil(
+    caches.keys()
+      .then(keys => Promise.all(keys.filter(key => key !== CACHE).map(key => caches.delete(key))))
+      .then(() => self.clients.claim())
+  );
+});
+
+self.addEventListener("fetch", event => {
+  if (event.request.method !== "GET") return;
+  event.respondWith(
+    fetch(event.request)
+      .then(response => {
+        const copy = response.clone();
+        caches.open(CACHE).then(cache => cache.put(event.request, copy));
+        return response;
+      })
+      .catch(() => caches.match(event.request).then(cached => cached || caches.match("./index.html")))
+  );
+});
